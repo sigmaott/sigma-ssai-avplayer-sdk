b83ce9faba7bfbb3847cbbfbe11eb8e5f6bb5a4e
haipn <haipn@gviet.vn>
2023-01-17 17:14:46 +0700

    update demo

---
+++ DemoSSAI/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  DemoSigmaInteractive
+//
+//  Created by PhamHai on 30/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtVideoUrl: UITextView!
+    @IBOutlet weak var btnOpenInteractive: UIButton!
+    @IBOutlet weak var txtError: UILabel!
+    var topSafeArea = 0.0
+    var bottomSafeArea = 0.0
+    override func viewDidLoad() {
+        let txtBorderColor: CGColor = UIColor.gray.cgColor;
+        let txtBorderRadius: CGFloat = 5.0, txtBorderWidth:CGFloat = 1.0;
+        txtVideoUrl.layer.borderWidth = txtBorderWidth;
+        txtVideoUrl.layer.borderColor = txtBorderColor;
+        txtVideoUrl.layer.cornerRadius = txtBorderRadius;
+        super.viewDidLoad()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if #available(iOS 11.0, *) {
+            topSafeArea = view.safeAreaInsets.top
+            bottomSafeArea = view.safeAreaInsets.bottom
+        } else {
+            topSafeArea = topLayoutGuide.length
+            bottomSafeArea = bottomLayoutGuide.length
+        }
+        // safe area values are now available to use
+    }
+    override func viewWillAppear(_ animated: Bool) {
+       super.viewWillAppear(animated)
+       AppUtility.lockOrientation(.portrait)
+   }
+
+   override func viewWillDisappear(_ animated: Bool) {
+       super.viewWillDisappear(animated)
+       AppUtility.lockOrientation(.all)
+   }
+    @IBAction func openInteractive(_ sender: UIButton) {
+        print("open interactive=>", txtVideoUrl.text!);
+        let videoUrl = txtVideoUrl.text!;
+        let isEmptyVideoUrl = videoUrl.trimmingCharacters(in: .whitespacesAndNewlines).count == 0 || (!videoUrl.hasPrefix("http://") && !videoUrl.hasPrefix("https://"));
+        if(isEmptyVideoUrl) {
+            txtError.text = "Value incorrect";
+        } else {
+            txtError.text = "";
+            self.view.endEditing(true);
+            let story = UIStoryboard(name: "Main", bundle: nil);
+            let controller = story.instantiateViewController(withIdentifier: "demoPlayer") as! PlayerViewController;
+//            let ViewController = ViewController(nibName: "PlayerViewController", bundle: nil);
+            controller.videoUrl = videoUrl;
+            controller.sessionUrl = videoUrl;
+            controller.bottomSafeArea = bottomSafeArea;
+            controller.topSafeArea = topSafeArea
+            self.navigationController?.pushViewController(controller, animated: true);
+//            self.present(controller, animated: true, completion: nil);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-sigma-ssai-avplayer-sdk
+# SSAITracking SDK
+
+## Requirement: IOS 12.4+
+
+### I. Cài đặt
+
+Thêm file SSAITracking.xcframework vào project.
+
+Project -> app target -> General -> Embedded Binaries
+
+![ssai_step_1](https://i.ibb.co/nR7v7H6/ssai-step-1.png)
+
+
+
+![ssai_step_2](https://i.ibb.co/Hq13d4c/ssai-step-2.jpg)
+
+
+
+![ssai_step_3](https://i.ibb.co/0QsP5r0/Screen-Shot-2023-01-17-at-13-40-40.png)
+
+
+
+![ssai_step_4](https://i.ibb.co/Z6PW1zL/ssai-step-4.jpg)
+
+
+
+### II. Sử dụng
+
+1. Thêm SSAITracking sdk vào project (mục **I**).
+
+2. Import SSAITracking vào file: 
+
+   ```swift
+   import SSAITracking
+   ```
+
+3. Tạo biến ssai type SigmaSSAI thể hiện cho view tương tác.
+
+   ```swift
+   var ssai: SigmaSSAI?;
+   ```
+
+   
+
+4. Khởi tạo sdk
+
+   ```swift
+   self.ssai = SSAITracking.SigmaSSAI.init(source: sessionUrl, trackingUrl, self, intervalTimeTracking)
+   ```
+
+   sessionUrl: Link session (Bắt buộc có nếu muốn sdk thực hiện việc gọi và lấy link video, để trống nếu app tự thực hiện việc lấy link video)
+
+   trackingUrl: Link tracking (Bắt buộc có nếu app thực hiện việc gọi link session và lấy link video, để trống nếu muốn  sdk thực hiện việc lấy link video)
+
+   self: implement SigmaSSAIInterface (Để lắng nghe các sự kiện sdk SSAI gọi để thực hiện các logic nếu cần)
+
+   intervalTimeTracking: Thời gian định kỳ sdk gọi tracking data (tính bằng giây)
+
+   1. Nếu dùng sdk thực hiện việc gọi và lấy link video
+
+   ​       1.1 - Lấy data url:  **self**.ssai?.getDataUrl() - return Dictonary["videoUrl": videoUrl, "trackingUrl": trackingUrl]
+
+   ​       1.2 - Set player cho sdk:  **self**.ssai?.setPlayer(videoPlayer!) - set sau khi khởi tạo xong player
+
+   2. Nếu app tự thực hiện việc gọi và lấy link video
+
+      2.1 - **self**.ssai?.onStartPlay() - gọi khi player sẵn sàng play
+
+      2.2 - self.ssai?.updatePlaybackTime(playbackTime: seconds) - gọi theo sự kiện update time play của player
+
+5. Các hàm listener
+
+     onSessionFail() - Khi sdk get data session fail
+
+     onTracking(_ message: String) - Khi sdk gọi 1 ads tracking request
+
+     onSessionUpdate(_ videoUrl: String) - Khi sdk cập nhật link video
+
+6. Public method
+
+   Clear() - Dùng để clear reset toàn bộ dữ liệu sdk
+
+   
+
